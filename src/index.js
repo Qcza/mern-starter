c1d881ff3ea5668e2b0e93d0a9c531e553bf8827
pku <pawel.kuczynski@ue.wroc.pl>
2017-07-05 07:07:50 +0200

    *update. add redux-logger on dev.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './js/reducers'
 import Root from './js/root';
 
-let store = createStore(reducers);
+const middlewares = [];
+if (process.env.NODE_ENV !== `production`) {
+    const { logger } = require(`redux-logger`);
+    middlewares.push(logger);
+}
+
+let store = createStore(
+    reducers,
+    applyMiddleware(...middlewares)
+);
 
 ReactDom.render(
     <Root store={store} />,
--- src/js/components/helloworld.js
+++ src/js/components/helloworld.js
@@ -7,7 +7,7 @@ class HelloWorld extends Component {
 
     render() {
         return (
-            <h1 style={{cursor: 'pointer'}} onClick={e=>this.props.changeColor(this.props.color === 'red' ? 'black' : 'red')} >Hello World!</h1>
+            <h1 title="click me!" style={{cursor: 'pointer', color: this.props.color}} onClick={e=>this.props.changeColor(this.props.color === 'red' ? 'black' : 'red')} >Hello World!</h1>
         )
     }
 
